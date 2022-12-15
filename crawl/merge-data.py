8abb2a14959d3e6ed3d161d6305846b3b332281d
Kilvia <kilvialeticia@hotmail.com>
2022-12-15 16:04:48 +0900

    Add function to remove tweets with no emojis

--- crawl/merge-data.py
+++ crawl/merge-data.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import glob
 import os
+import csv
 
-# path to the files
-files = os.path.join(".", "tweets-new-*.csv")
+def merge():
+    # path to the files
+    files = os.path.join(".", "tweets-new-*.csv")
 
-# list of merged files
-files = glob.glob(files)
+    # list of merged files
+    files = glob.glob(files)
 
-# joining files
-df = pd.concat(map(pd.read_csv, files), ignore_index=True)
-df.to_csv("data.csv", index=False)
-print(df)
+    # joining files
+    df = pd.concat(map(pd.read_csv, files), ignore_index=True)
+    df.to_csv("data.csv", index=False)
+
+def remove():
+    # read file
+    df = pd.read_csv("data.csv")
+    print(df)
+    # drop tweets without emoji
+    df = df[df['emojis'].ne('[]')]
+    print(df)
+    # write the data to a new dataset
+    df.to_csv("data-rm.csv", index=False)
+
+remove()
